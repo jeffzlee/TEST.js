f1d98e1dda2ff8feb00b02a094f2c62d7de67c23
jeffzlee <jeffzlee@163.com>
2017-11-13 23:48:56 +0800

    Update TEST.js

--- TEST.js
+++ TEST.js
@@ -1,14 +1,11 @@
 function TEST(pin) {
   this.pin = pin;
-  getPinMode(pin) {
-  console.log(getPinMode(pin));
-};
-};
-/*
+  };
+
 TEST.prototype.getPinMode = function (pin) {
   console.log(getPinMode(pin));
 };
-*/
+
 exports.connect = function(pin) {
     return new TEST(pin);
 };

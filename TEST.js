9424adbcf4652c871ee1539c23fecc38f29cd636
jeffzlee <jeffzlee@163.com>
2017-11-14 00:43:10 +0800

    Update TEST.js

--- TEST.js
+++ TEST.js
@@ -2,6 +2,7 @@ function TEST(pin) {
   this.pin = pin;
   }
 TEST.prototype.pinMode = function (interval) {
+  console.log(interval);
   setInterval(function () {}, interval);
   console.log(getPinMode(this.pin));
   };

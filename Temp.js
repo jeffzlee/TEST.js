5d4eab0dd64642483d93641b67697071724eaf40
jeffzlee <jeffzlee@163.com>
2017-11-14 13:05:18 +0800

    Update Temp.js

--- Temp.js
+++ Temp.js
@@ -1,11 +1,6 @@
 function Temp(temp) {
-  this.temp = temp;
-  }
-TEST.prototype.printTemp = function (interval) {
-  setTimeout(function () {console.log(temp);}, interval);
-  
-  };
-
-exports.connect = function(tmpe) {
-    return new Temp(temp);
+  this.temp = temp;}
+TEST.prototype.printTemp=function (interval){
+  setTimeout(function(){console.log(temp);},interval);
 };
+exports.connect = function(tmpe) {return new Temp(temp);};

3008d48b28017d6e092047b1348191e083ad5bfe
jeffzlee <jeffzlee@163.com>
2017-11-14 12:25:23 +0800

    Create Temp.js

---
+++ Temp.js
@@ -0,0 +1,11 @@
+function Temp(temp) {
+  this.temp = temp;
+  }
+TEST.prototype.printTemp = function (interval) {
+  setTimeout(function () {console.log(temp);}, interval);
+  
+  };
+
+exports.connect = function(tmpe) {
+    return new Temp(temp);
+};
